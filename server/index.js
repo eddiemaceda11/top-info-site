cff32a440bbfedcf535b4715582c3ac2fb9af9ff
Eddie Maceda <eddiemaceda11@gmail.com>
2024-10-29 21:58:07 -0700

    Create the middleware to handle invalid requests and redirect to a 404 page

--- server/index.js
+++ server/index.js
@@ -13,6 +13,17 @@ app.get("/contact-us", (req, res) => {
   res.sendFile("/Users/eddiemaceda/top-info-site/server/contact-us.html");
 });
 
+/*
+  Below are two separate options for how to handle a request to an invalid url.
+  We want to make sure these are below all of out other routes -> 404 handling as absolute last thing.
+  You can use either middleware, or a catch all route.
+  */
+
+// Middleware
+app.use((req, res) => {
+  res.sendFile("/Users/eddiemaceda/top-info-site/server/404.html");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}`);

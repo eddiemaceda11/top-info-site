f003cae301be0d0898080f6d042e4dfcec5bfc5d
Eddie Maceda <eddiemaceda11@gmail.com>
2024-10-29 21:56:58 -0700

    Create route for the contact-us page

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ app.get("/about", (req, res) => {
   res.sendFile("/Users/eddiemaceda/top-info-site/server/about.html");
 });
 
+app.get("/contact-us", (req, res) => {
+  res.sendFile("/Users/eddiemaceda/top-info-site/server/contact-us.html");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}`);

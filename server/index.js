067e3d7fa2f26638cc276335fb091b0fff554b76
Eddie Maceda <eddiemaceda11@gmail.com>
2024-10-29 21:56:14 -0700

    Create route for the about page

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.sendFile("/Users/eddiemaceda/top-info-site/server/index.html");
 });
 
+app.get("/about", (req, res) => {
+  res.sendFile("/Users/eddiemaceda/top-info-site/server/about.html");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}`);
